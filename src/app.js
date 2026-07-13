acd8f510bf1a76209fa4ca65b956c622f7655e68
ziauddin14 <ziamaryam2526@gmail.com>
2026-07-14 00:36:09 +0500

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://digitalpersonalsecretory.vercel.app'
+    'https://digitalpersonelsecretory.vercel.app'
   ]
 }));
 app.use(express.json());
